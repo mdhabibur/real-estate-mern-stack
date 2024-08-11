209d016a12340653b8ef55c7e8af1a5790d859c3
mdhabibur <mdhrahman532@gmail.com>
2024-08-11 16:02:44 +0600

    in root node package also initialized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
 
   )
 }
