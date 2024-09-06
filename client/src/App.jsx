382ec1128ad15f7ec809c322995a601bc71cb0b6
mdhabibur <mdhrahman532@gmail.com>
2024-09-06 19:55:41 +0600

    Fix listingDetails file path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,9 @@ import Header from "./components/header/Header";
 import PrivateRoute from "./components/privateRoutes/PrivateRoute";
 import PublicRoute from "./components/privateRoutes/PublicRoute";
 import CreateListing from "./pages/CreateListing";
-import ListingDetails from "./pages/listingDetails";
-import UpdateListing from "./pages/UpdateListing";
+import ListingDetails from './pages/ListingDetails'
+import UpdateListing from './pages/UpdateListing'
+
 import Search from "./pages/Search";
 
 function App() {
@@ -65,8 +66,24 @@ function App() {
 				/>
 
 				<Route path="/about" element={<About />} />
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/signup" element={<SignUp />} />
+
+				<Route
+					path="/signin"
+					element={
+						<PublicRoute>
+							<SignIn />
+						</PublicRoute>
+					}
+				/>
+
+				<Route
+					path="/signup"
+					element={
+						<PublicRoute>
+							<SignUp />
+						</PublicRoute>
+					}
+				/>
 			</Routes>
 		</Router>
 	);
